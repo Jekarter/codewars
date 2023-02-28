9a5ae587e3f5971f89efbe0a58b3ab7c6871f824
Jekarter <jkartyalta@yandex.ru>
2023-02-28 13:00:37 +0500

    61

---
+++ sum-of-a-sequence-hard-core.js
@@ -0,0 +1,23 @@
+/* 
+6kyu Sum of a Sequence [Hard-Core Version]
+
+As the title suggests, this is the hard-core version of another neat kata.
+
+The task is simple to explain: simply sum all the numbers from the first parameter being the beginning to the second parameter being the upper limit (possibly included), going in steps expressed by the third parameter:
+
+sequenceSum(2,2,2) === 2
+sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+sequenceSum(1,5,3) === 5 // 1 + 4
+If it is an impossible sequence (with the beginning being larger the end and a positive step or the other way around), just return 0. See the provided test cases for further examples :)
+
+Note: differing from the other base kata, much larger ranges are going to be tested, so you should hope to get your algo optimized and to avoid brute-forcing your way through the solution.
+
+*ALGORITHMS 
+*/
+
+
+function sequenceSum(begin, end, step){
+  let k = Math.floor((end - begin) / step);
+  return k < 0 ? 0 : Math.floor((k + 1) * (k * step + begin + begin) / 2)
+}
